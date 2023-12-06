97ac534d6558f8bee8cb4c69dba7f273ae0de2d4
liconaj <liconaj215@gmail.com>
2023-12-05 21:19:58 -0500

    Comentarios y legibilidad código; mostrar ángulo deformación total

--- torsionsys.m
+++ torsionsys.m
@@ -1,43 +1,53 @@
 classdef torsionsys
-    %TORSIONSYS Summary of this class goes here
-    %   Detailed explanation goes here
+    %TORSIONSYS solucionador problema de torsión en una viga
+    %   Solucionador de problema de torsión en vigas de sección regular
+
     properties (GetAccess = public)
-        mesh
-        meshindex
-        sketch
-        matA
-        vecF
-        Phi
-        imgPhi
-        theta
-        tauxz
-        tauyz
-        tau
-        J, W, L, G, h, T
+        % Propiedades públicas, accesibles desde fuera de la clase
+        mesh        % Malla
+        meshindex   % Índice de la malla
+        sketch      % Boceto
+        matA        % Matriz A
+        vecF        % Vector F
+        Phi         % Función Phi
+        imgPhi      % Imagen de Phi
+        theta       % Ángulo de deformación por unidad de distancia
+        gamma       % Ángulo de deformación total
+        tauxz       % Componente tau_xz
+        tauyz       % Componente tau_yz
+        tau         % Magnitud de tau
+        J, W, L, G, h, T  % Parámetros y propiedades del sistema
     end
 
     properties (GetAccess = private)
-        targetT
-        profile_data
-        n % numero de nodos
-        m % tamaño matriz
+        % Propiedades privadas, solo accesibles dentro de la clase
+        targetT         % Valor objetivo de T (Torque) 
+        profile_data    % Datos del perfil
+        n               % Número de nodos
+        m               % Tamaño de matriz
     end
 
     methods
         function obj = torsionsys(profile_data, params)
-            %TORSIONSYS Construct an instance of this class
-            %   Detailed explanation goes here
+            %TORSIONSYS Crear instancia de la clase
+            %   Inicializa las propiedades con datos dados
             obj.profile_data = profile_data;
             obj.W = params.W;
             obj.L = params.L;
             obj.G = params.G;
             obj.h = params.h;
             obj.targetT = params.T;
-            obj = obj.setupemesh();
-            obj.J = obj.calcJ();
+            obj = obj.setupemesh(); % Configura la malla
+            obj.J = obj.calcJ();    % Calcula el momento polar de inercia
         end
+
         function obj = setupsystem(obj)
+            %SETUPSYSTEM Configuración del sistema de ecuaciones: define la
+            %matriz A para resolver el problema
+
+            % Inicializa la matriz A con valores específicos
             obj.matA = -4*speye(obj.n);
+            % Recorre la malla y asigna coeficientes a la matriz A
             for y = 1:obj.m
                 for x = 1:obj.m
                     ii = obj.meshindex(y, x);
@@ -59,12 +69,20 @@ classdef torsionsys
                 end
             end
         end
+
         function obj = solvesystem(obj)
+            %SOLVESYSTEM Resuelve el sistema de ecuaciones para encontrar
+            %la distribución de Phi y otros valores.
             eq = @(T) obj.calcT(T) - obj.targetT;
             Ttheta = fsolve(eq, obj.targetT, optimoptions(@fsolve, "Display", "off"));
             obj.theta = Ttheta / (obj.J * obj.G);
+            obj.gamma = obj.theta * obj.L;
             obj.Phi = obj.calcPhi(obj.theta);
             obj.T = 2*sum(obj.Phi) * obj.h^2;
+        end
+
+        function obj = arrangesolution(obj)
+            %ARRANGESOLUTION Organiza la distribución de Phi en la malla
             obj.imgPhi = obj.mesh;
             for y = 1:obj.m
                 for x = 1:obj.m
@@ -75,7 +93,9 @@ classdef torsionsys
                     obj.imgPhi(y, x) = obj.Phi(idx);
                 end
             end
+            % invertir columnas para visualización
             obj.imgPhi = obj.imgPhi(:,end:-1:1);
+            % Calcular las componentes tau_xz, tau_yz
             obj.tauxz = nan(obj.m-2);
             obj.tauyz = nan(obj.m-2);
             for y = 2:obj.m-1
@@ -89,39 +109,52 @@ classdef torsionsys
                     obj.tauyz(y-1,x-1) = -dPhiy/(2*obj.h);
                 end
             end
+            % Calcula las magnitudes de tau
             obj.tau = (obj.tauxz.^2+obj.tauyz.^2).^0.5;
         end
+
         function Phi = calcPhi(obj, theta)
+            %CALCPHI Calcular el vector solución Phi
             F = -2*obj.G*theta;
             obj.vecF = F * ones(obj.n, 1);
             Phi = obj.matA\obj.vecF;
         end
+
         function T = calcT(obj, Ttheta)
+            %CALCT Calcular el torque T a partir del vector Phi dada por
+            %Ttheta
             theta_guess = Ttheta / (obj.J * obj.G);
             T = 2*sum(obj.calcPhi(theta_guess)) * obj.h^2;
         end
+
         function showPhi(obj)
+            %SHOWPHI Mostrar distribución de Phi en la geometría
             figure
             xt = (1:obj.m)*obj.h;
             yt = (1:obj.m)*obj.h;
             contourf(xt,yt,obj.imgPhi, 20, 'LineColor', 'flat')
             title("Distribución \Phi")
             colorbar
-            daspect([1 1 1])
-            % set(gca,'Color','k')
+            daspect([1 1 1]) % Ajustar proporción ejes de la gráfica
+            % set(gca,'Color','k') % Cambia el color de fondo (opcional)
         end
+
         function showtau(obj)
+            %SHOWTAU Mostrar magnitud esfuerzos cortantes en geometría
             figure
             xt = (2:obj.m-1)*obj.h;
             yt = (2:obj.m-1)*obj.h;
             contourf(yt,xt,obj.tau, 20, 'LineColor', 'flat')
             title("Distribución magnitud \tau")
             hc = colorbar;
-            title(hc, "GPa")
+            title(hc, "GPa") % Mostrar unidad esfuerzo
             colormap turbo
-            daspect([1 1 1])
+            daspect([1 1 1]) % Ajustar proporción ejes de la gráfica
         end
+
         function showtaucamp(obj)
+            %SHOWTAUCAMP Mostrar vectores de esfuerzos cortantes en la
+            %geometría
             figure
             p = (obj.m-3)/17;
             xt = (2:p:obj.m-1)*obj.h;
@@ -135,11 +168,13 @@ classdef torsionsys
             contour(xt,yt,obj.tau,20, 'LineColor', 'flat')
             title("Distribución vectores \tau")
             hc = colorbar;
-            title(hc, "GPa")
+            title(hc, "GPa") % Mostrar unidad esfuerzo
             colormap turbo
-            daspect([1 1 1])
+            daspect([1 1 1]) % Ajustar proporción ejes de la gráfica
         end
         function showtaucomps(obj)
+            %SHOWTAUCOMPS Mostrar magnitudes de las componentes de los
+            %esfuerzos cortantes
             figure
             xt = (2:obj.m-1)*obj.h;
             yt = (2:obj.m-1)*obj.h;
@@ -148,27 +183,30 @@ classdef torsionsys
             contourf(xt,yt,obj.tauxz(end:-1:1,:), 20, 'LineColor', 'flat')
             title("\tau_xz")
             hc = colorbar;
-            title(hc, "GPa")
+            title(hc, "GPa") % Mostrar unidad esfuerzo
             colormap turbo
-            daspect([1 1 1])
+            daspect([1 1 1]) % Ajustar proporción ejes de la gráfica
             
             subplot(1,2,2)
             contourf(xt,yt,obj.tauyz(end:-1:1,:), 20, 'LineColor', 'flat')
             title("\tau_yz")
             hc = colorbar;
-            title(hc, "GPa")
+            title(hc, "GPa") % Mostrar unidad esfuerzo
             colormap turbo
-            daspect([1 1 1])
+            daspect([1 1 1]) % Ajustar proporción ejes de la gráfica
         end
     end
     methods (Access = private)
         function obj = setupemesh(obj)
+            % Función privada para configurar la malla de puntos
             size = length(obj.profile_data);
-            cellsize = obj.W/size;
-            scale = cellsize/obj.h;
+            cellsize = obj.W/size; % Tamaño de división
+            scale = cellsize/obj.h; % Escala para el tamaño de la malla
+            % Escalar datos a número de celdas
             obj.sketch = imresize(obj.profile_data, scale, "nearest");
             obj.m = length(obj.sketch) + 1;
-            rawmesh = nan(obj.m);
+            rawmesh = nan(obj.m); % Inicialización de la malla
+            % Creación de la malla a partir del celdas
             for y = 1:obj.m
                 for x = 1:obj.m
                     if x == 1
@@ -185,8 +223,8 @@ classdef torsionsys
                     else
                         ys = [y-1, y];
                     end
-                    neighbors = obj.sketch(ys, xs);
-                    val = sum(neighbors, "all");
+                    neighbors = obj.sketch(ys, xs); % Vecinos en el perfil
+                    val = sum(neighbors, "all"); % Suma de los vecinos
                     if val > 2 % && ~any(isnan(neighbors(:)))
                         rawmesh(y,x) = 1;
                     elseif val > 0
@@ -214,18 +252,20 @@ classdef torsionsys
         end
 
         function J = calcJ(obj)
-            J = 0;
-            dA = obj.h^2;
-            cx = obj.W/2;
-            cy = obj.W/2;
-            [sm, sn] = size(obj.sketch);
+            % Función privada para calcular el momento polar de inercia J
+            J = 0; % Inicialización del momento polar de inercia
+            dA = obj.h^2; % Área diferencial dA
+            cx = obj.W/2; % Coordenada x del centro del perfil
+            cy = obj.W/2; % Coordenada y del centro del perfil
+            [sm, sn] = size(obj.sketch); % Número de dA discretizadas
             for jj = 1:sm
                 for ii = 1:sn
                     if ~obj.sketch(jj,ii)
                         continue
                     end
-                    x = ii*obj.h;
-                    y = jj*obj.h;
+                    x = ii*obj.h; % Coordenada x de dA en la malla
+                    y = jj*obj.h; % Coordenada y de dA en la malla
+                    % Distancia al cuadrado entre dA y el centro del perfil
                     r2 = (x-cx)^2+(y-cy)^2;
                     J = J + r2 * dA;
                 end
@@ -234,7 +274,8 @@ classdef torsionsys
     end
     methods (Static)
         function coefs = getadjcoefs(ncase)
-            ntype = ncase(2,2);
+            % Obtener coeficientes de nodos adyacentes
+            ntype = ncase(2,2);  % Tipo de nodo
             switch ntype
                 case 4
                     coefs = [1 1 1 1];
@@ -245,22 +286,24 @@ classdef torsionsys
             end
         end
         function coefs = shiftcoefs(nodescase, defcoefs, nodeseek, seekcorner)
+            % Cambiar coeficientes según rotación del caso
+            % Explicación:
             % adjnodes => [1 2 3 4]
             % "side"  . 2 .   "corner"   1 . 2
             %         1 . 3              . . .
             %         . 4 .              4 . 3
             adjnodes = zeros(1, 4);
-            y = 2; x = 2;
+            y = 2; x = 2; % Coordenadas nodo central en caso
             if seekcorner
-                adjnodes(1) = nodescase(y-1, x-1);
-                adjnodes(2) = nodescase(y-1, x+1);
-                adjnodes(3) = nodescase(y+1, x+1);
-                adjnodes(4) = nodescase(y+1, x-1);
+                adjnodes(1) = nodescase(y-1, x-1); % Nodo superior izquierdo
+                adjnodes(2) = nodescase(y-1, x+1); % Nodo superior derecho
+                adjnodes(3) = nodescase(y+1, x+1); % Nodo inferior derecho
+                adjnodes(4) = nodescase(y+1, x-1); % Nodo inferior izquierdo
             else
-                adjnodes(1) = nodescase(y, x-1);
-                adjnodes(2) = nodescase(y-1, x);
-                adjnodes(3) = nodescase(y, x+1);
-                adjnodes(4) = nodescase(y+1, x);
+                adjnodes(1) = nodescase(y, x-1); % Nodo izquierdo
+                adjnodes(2) = nodescase(y-1, x); % Nodo superior
+                adjnodes(3) = nodescase(y, x+1); % Nodo derecho
+                adjnodes(4) = nodescase(y+1, x); % Nodo inferior
             end
             shiftsteps = find(adjnodes == nodeseek) - 1;
             coefs = circshift(defcoefs, shiftsteps);
